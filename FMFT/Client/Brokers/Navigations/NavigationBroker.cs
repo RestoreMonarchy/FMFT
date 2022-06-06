754a864b8efe22885b762865a871f18b08b5aa4a
MCrow <michalwrona@hotmail.com>
2022-06-06 19:09:13 +0200

    Add project files.

---
+++ FMFT/Client/Brokers/Navigations/NavigationBroker.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Routing;
+
+namespace FMFT.Client.Brokers.Navigations
+{
+    public class NavigationBroker : INavigationBroker
+    {
+        private readonly NavigationManager navigationManager;
+
+        public NavigationBroker(NavigationManager navigationManager)
+        {
+            this.navigationManager = navigationManager;
+            navigationManager.LocationChanged += TriggerLocationChanged;
+        }
+
+        public string Uri => navigationManager.Uri;
+        public string AbsoluteUri => navigationManager.ToAbsoluteUri(Uri).ToString();
+        public string RelativeUri => navigationManager.ToBaseRelativePath(Uri);
+
+        public event Action<LocationChangedEventArgs> OnLocationChange;
+        private void TriggerLocationChanged(object sender, LocationChangedEventArgs e)
+        {
+            OnLocationChange?.Invoke(e);
+        }
+    }
+}
---
+++ FMFT/Client/wwwroot/js/app.js
@@ -0,0 +1,23 @@
+﻿
+function DoPanzoom() {
+    const elem = document.getElementById('panzoom-element')
+    const panzoom = Panzoom(elem, {
+        maxScale: 5
+    })
+    /*panzoom.pan(1000, 10)*/
+    panzoom.zoom(0.5, { animate: true })
+
+    const zoomInButton = document.getElementById('panzoom-zoomin');
+    const zoomOutButton = document.getElementById('panzoom-zoomout');
+    const resetButton = document.getElementById('panzoom-reset');
+
+    zoomInButton.addEventListener('click', panzoom.zoomIn)
+    zoomOutButton.addEventListener('click', panzoom.zoomOut)
+    resetButton.addEventListener('click', panzoom.reset)
+
+    // Panning and pinch zooming are bound automatically (unless disablePan is true).
+    // There are several available methods for zooming
+    // that can be bound on button clicks or mousewheel.
+    //button.addEventListener('click', panzoom.zoomIn)
+    //elem.parentElement.addEventListener('wheel', panzoom.zoomWithWheel)
+}
