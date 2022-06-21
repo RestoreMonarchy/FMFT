3e43ba228562101a7c85b161fa3225e3f00d31f4
MCrow <michalwrona@hotmail.com>
2022-06-21 22:46:55 +0200

    Add PostDeployment script

---
+++ web/Client/Views/Components/Shows/Selectors/SeatSelector.razor.cs
@@ -0,0 +1,30 @@
+﻿using FMFT.Web.Client.Brokers.JSRuntimes;
+using FMFT.Web.Shared.Models.Auditoriums;
+using FMFT.Web.Shared.Models.Seats;
+using Microsoft.AspNetCore.Components;
+
+namespace FMFT.Web.Client.Views.Components.Shows.Selectors
+{
+    public partial class SeatSelector
+    {
+        [Parameter]
+        public Auditorium Auditorium { get; set; }
+
+        public IEnumerable<IGrouping<short, Seat>> RowSeats 
+            => Auditorium.Seats.OrderBy(x => x.Number).
+            GroupBy(x => x.Row);
+
+        [Inject]
+        public IJSRuntimeBroker JSRuntimeBroker { get; set; }
+
+        public ElementReference ContainerElement { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            if (firstRender)
+            {
+                await JSRuntimeBroker.InitializePanzoomElementAsync(ContainerElement);
+            }
+        }
+    }
+}
