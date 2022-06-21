4b4bfd5100ae8b835a0dca2f4967e4ad1229ff33
MCrow <michalwrona@hotmail.com>
2022-06-21 23:27:57 +0200

    Add ability to select seats with the SeatSelector

---
+++ web/Client/Views/Components/Shows/Cards/SeatReservationCard.razor.cs
@@ -0,0 +1,14 @@
+﻿using FMFT.Web.Shared.Models.Auditoriums;
+using FMFT.Web.Shared.Models.Seats;
+using Microsoft.AspNetCore.Components;
+
+namespace FMFT.Web.Client.Views.Components.Shows.Cards
+{
+    public partial class SeatReservationCard
+    {
+        [Parameter]
+        public Auditorium Auditorium { get; set; }
+
+        public List<Seat> SelectedSeats { get; set; } = new();
+    }
+}
---
+++ web/Client/Views/Components/Shows/Cards/ShowInformationCard.razor.cs
@@ -0,0 +1,14 @@
+﻿using FMFT.Web.Shared.Models.Auditoriums;
+using FMFT.Web.Shared.Models.Shows;
+using Microsoft.AspNetCore.Components;
+
+namespace FMFT.Web.Client.Views.Components.Shows.Cards
+{
+    public partial class ShowInformationCard
+    {
+        [Parameter]
+        public Show Show { get; set; }
+        [Parameter]
+        public Auditorium Auditorium { get; set; }
+    }
+}
