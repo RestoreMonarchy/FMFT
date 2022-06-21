0de47716f628468210c5421a4c32a8bc28c41532
MCrow <michalwrona@hotmail.com>
2022-06-21 18:07:14 +0200

    Add ShowId, UserId and IsConfirmed columns

--- web/Shared/Models/Reservations/Reservation.cs
+++ web/Shared/Models/Reservations/Reservation.cs
@@ -1,11 +1,11 @@
-﻿using FMFT.Web.Shared.Models.Seats;
-
-namespace FMFT.Web.Shared.Models.Reservations
+﻿namespace FMFT.Web.Shared.Models.Reservations
 {
     public class Reservation
     {
         public int Id { get; set; }
+        public int ShowId { get; set; }
         public int SeatId { get; set; }
+        public int UserId { get; set; }
         public DateTime CreateDate { get; set; }
     }
 }
