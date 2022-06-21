0588d0b40983d02d3f4c8acfda7bbd4eeff4e26b
MCrow <michalwrona@hotmail.com>
2022-06-22 00:16:27 +0200

    Add CreateReservation stored procedure and ReservationStatus

--- database/dbo/tables/Reservations.sql
+++ database/dbo/tables/Reservations.sql
@@ -4,6 +4,6 @@
 	ShowId INT NOT NULL CONSTRAINT FK_Reservations_ShowId FOREIGN KEY REFERENCES dbo.Shows(Id),
 	SeatId INT NOT NULL CONSTRAINT FK_Reservations_SeatId FOREIGN KEY REFERENCES dbo.Seats(Id),
 	UserId INT NOT NULL CONSTRAINT FK_Reservations_UserId FOREIGN KEY REFERENCES dbo.Users(Id),
-	IsConfirmed BIT NOT NULL CONSTRAINT DF_Reservations_IsConfirmed DEFAULT 0,
+	[Status] INT NOT NULL CONSTRAINT DF_Reservations_Status DEFAULT 0,
 	CreateDate DATETIME2(0) NOT NULL CONSTRAINT DF_Reservations_CreateDate DEFAULT SYSDATETIME()
 )
---
+++ web/Shared/Models/Reservations/Params/CreateReservationParams.cs
@@ -0,0 +1,9 @@
+﻿namespace FMFT.Web.Shared.Models.Reservations.Params
+{
+    public class CreateReservationParams
+    {
+        public int ShowId { get; set; }
+        public int SeatId { get; set; }
+        public int UserId { get; set; }
+    }
+}
