7941b3dd2ebf94ef4be458d0923068fb2cca05d5
MCrow <michalwrona@hotmail.com>
2022-07-11 22:00:37 +0200

    Update CreateReservation stored procedure

--- database/dbo/procedures/CreateReservation.sql
+++ database/dbo/procedures/CreateReservation.sql
@@ -7,30 +7,29 @@ BEGIN
 	SET NOCOUNT ON;
 	SET XACT_ABORT ON;
 
-	--DECLARE @auditoriumId INT;
-	
-	--IF NOT EXISTS(SELECT * FROM dbo.Seats)
-
-	--SELECT @auditoriumId = AuditoriumId FROM dbo.Shows WHERE Id = @ShowId;
-
-	--IF NOT EXISTS(SELECT * FROM dbo.Seats WHERE Id = @SeatId AND AuditoriumId = @auditoriumId)
-	--	RETURN 1;
+	DECLARE @ret INT = 0;
+	DECLARE @id INT;
 
 	IF EXISTS(SELECT * FROM dbo.Reservations WHERE ShowId = @ShowId AND SeatId = @SeatId)
-		RETURN 1;
+		SET @ret = 1;
 
 	IF EXISTS(SELECT * FROM dbo.Reservations WHERE UserId = @UserId AND ShowId = @ShowId)
-		RETURN 2;
+		SET @ret = 2;
 
-	INSERT INTO dbo.Reservations (ShowId, SeatId, UserId)
-	VALUES (@ShowId, @SeatId, @UserId);
+	IF @ret = 0
+	BEGIN
+		INSERT INTO dbo.Reservations (ShowId, SeatId, UserId)
+		VALUES (@ShowId, @SeatId, @UserId);
+
+		SET @id = SCOPE_IDENTITY();
+	END;
 
 	SELECT r.*, s.*, se.*, u.* 
 	FROM dbo.Reservations r
 	JOIN dbo.Shows s ON s.Id = r.ShowId
 	JOIN dbo.Seats se ON se.Id = r.SeatId
 	JOIN dbo.Users u ON u.Id = r.UserId
-	WHERE r.Id = SCOPE_IDENTITY();
+	WHERE r.Id = @id;
 
-	RETURN 0;
-END
+	RETURN @ret;
+END;
