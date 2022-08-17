cb7ab4b5c134237266979065501d66a423a7336e
MCrow <michalwrona@hotmail.com>
2022-08-17 12:45:22 +0200

    Add RestfulSense project

---
+++ external/RESTFulSense.WebAssembly/Models/ValidationProblemDetails.cs
@@ -0,0 +1,25 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) Brian Parker & Hassan Habib
+// Licensed under the MIT License.
+// See License.txt in the project root for license information.
+// ---------------------------------------------------------------
+
+using System;
+using System.Collections.Generic;
+using System.Text.Json.Serialization;
+
+namespace RESTFulSense.WebAssembly.Exceptions
+{
+    public class ValidationProblemDetails
+    {
+        public ValidationProblemDetails()
+        {
+            Errors = new Dictionary<string, string[]>(StringComparer.Ordinal);
+        }
+
+        public string Title { get; set; }
+        [JsonPropertyName("errors")]
+        public IDictionary<string, string[]> Errors { get; set; }
+        public string Type { get; set; }
+    }
+}
