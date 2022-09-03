27f6e388abfcbe93a791c717ebfed347da934d04
MCrow <michalwrona@hotmail.com>
2022-09-03 13:06:36 +0200

    Add reservation actions card

---
+++ web/Client/Views/Components/Reservations/Cards/ReservationActionsCard.razor.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FMFT.Web.Client.Views.Components.Reservations.Cards
+{
+    public partial class ReservationActionsCard
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+    }
+}
