08360c6c09aae0e3300b8e7afa02dd4280f465dc
MCrow <michalwrona@hotmail.com>
2022-10-22 14:51:03 +0200

    Update the LoadingView and add auditorium to show page

--- extensions/blazor/Bases/Loadings/LoadingView.razor.cs
+++ extensions/blazor/Bases/Loadings/LoadingView.razor.cs
@@ -5,17 +5,20 @@ namespace FMFT.Extensions.Blazor.Bases.Loadings
     public partial class LoadingView
     {
         [Parameter]
-        public object State { get; set; }
-
-        [Parameter]
         public RenderFragment ChildContent { get; set; }
 
-        private bool CheckState()
-        {
-            if (State == default)
-                return false;
+        public bool IsLoading { get; set; } = true;
 
-            return true;
+        public void StartLoading()
+        {
+            IsLoading = true;
+            InvokeAsync(StateHasChanged);
+        }
+
+        public void StopLoading()
+        {
+            IsLoading = false;
+            InvokeAsync(StateHasChanged);
         }
     }
 }
--- web/Client/Views/Pages/Home/ShowPage.razor.cs
+++ web/Client/Views/Pages/Home/ShowPage.razor.cs
@@ -1,4 +1,5 @@
-﻿using FMFT.Web.Client.Models.API;
+﻿using FMFT.Extensions.Blazor.Bases.Loadings;
+using FMFT.Web.Client.Models.API;
 using FMFT.Web.Client.Models.API.Auditoriums;
 using FMFT.Web.Client.Models.API.Shows;
 using Microsoft.AspNetCore.Components;
@@ -12,14 +13,22 @@ namespace FMFT.Web.Client.Views.Pages.Home
 
         private string ShowName => ShowResponse?.Object.Name ?? ShowId.ToString();
 
+        private LoadingView LoadingView { get; set; }
+
         public APIResponse<Show> ShowResponse { get; set; }
         public APIResponse<Auditorium> AuditoriumResponse { get; set; }
 
         public Show Show => ShowResponse.Object;
+        public Auditorium Auditorium => AuditoriumResponse.Object;
 
         protected override async Task OnParametersSetAsync()
         {
             ShowResponse = await APIBroker.GetShowByIdAsync(ShowId);
+            if (ShowResponse.IsSuccessfull)
+            {
+                AuditoriumResponse = await APIBroker.GetAuditoriumByIdAsync(Show.AuditoriumId);
+            }
+            LoadingView.StopLoading();
         }
     }
 }
