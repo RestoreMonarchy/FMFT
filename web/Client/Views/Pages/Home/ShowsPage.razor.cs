3030f6197fae3f2fe402f45c3e59b5b95c01f503
MCrow <michalwrona@hotmail.com>
2022-10-22 13:36:57 +0200

    Add shows page

---
+++ web/Client/Views/Pages/Home/ShowsPage.razor.cs
@@ -0,0 +1,15 @@
+﻿using FMFT.Web.Client.Models.API;
+using FMFT.Web.Client.Models.API.Shows;
+
+namespace FMFT.Web.Client.Views.Pages.Home
+{
+    public partial class ShowsPage
+    {
+        public APIResponse<List<Show>> ShowsResponse { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ShowsResponse = await APIBroker.GetAllShowsAsync();
+        }
+    }
+}
