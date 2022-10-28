ee329d6dda348ada967d471369eaf7c0d18ba67d
MCrow <michalwrona@hotmail.com>
2022-10-29 01:31:17 +0200

    Add account reservations page

---
+++ web/Client/Views/Pages/Account/AccountReservationsPage.razor.cs
@@ -0,0 +1,21 @@
+﻿using FMFT.Extensions.Blazor.Bases.Loadings;
+using FMFT.Web.Client.Models.API;
+using FMFT.Web.Client.Models.API.Reservations;
+
+namespace FMFT.Web.Client.Views.Pages.Account
+{
+    public partial class AccountReservationsPage
+    {
+        public LoadingView LoadingView { get; set; }
+
+        public APIResponse<List<Reservation>> ReservationsResponse { get; set; }
+
+        public List<Reservation> Reservations => ReservationsResponse.Object;
+
+        protected override async Task OnInitializedAsync()
+        {
+            ReservationsResponse = await APIBroker.GetUserReservationsAsync(UserAccountState.UserAccount.UserId);
+            LoadingView.StopLoading();
+        }
+    }
+}
