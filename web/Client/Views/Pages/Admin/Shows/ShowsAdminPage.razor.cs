85dca7cb234ea0b0c4950ebffadc0e728a5d76e3
MCrow <michalwrona@hotmail.com>
2022-11-07 22:17:47 +0100

    Add admin shows table

---
+++ web/Client/Views/Pages/Admin/Shows/ShowsAdminPage.razor.cs
@@ -0,0 +1,21 @@
+﻿using FMFT.Extensions.Blazor.Bases.Loadings;
+using FMFT.Web.Client.Models.API;
+using FMFT.Web.Client.Models.API.Shows;
+
+namespace FMFT.Web.Client.Views.Pages.Admin.Shows
+{
+    public partial class ShowsAdminPage
+    {
+        public LoadingView LoadingView { get; set; }
+
+        public List<Show> Shows => ShowsResponse.Object;
+
+        public APIResponse<List<Show>> ShowsResponse { get; set; }
+
+        protected override async Task OnInitializedAsync()
+        {
+            ShowsResponse = await APIBroker.GetAllShowsAsync();
+            LoadingView.StopLoading();
+        }
+    }
+}
