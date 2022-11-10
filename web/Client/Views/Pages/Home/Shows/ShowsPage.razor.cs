60538b152bb11be8f6f00c6718730c8431e522e1
MCrow <michalwrona@hotmail.com>
2022-11-10 22:35:41 +0100

    Add show thumbnails to shows page

--- web/Client/Views/Pages/Home/Shows/ShowsPage.razor.cs
+++ web/Client/Views/Pages/Home/Shows/ShowsPage.razor.cs
@@ -1,6 +1,7 @@
 ﻿using FMFT.Extensions.Blazor.Bases.Loadings;
 using FMFT.Web.Client.Models.API;
 using FMFT.Web.Client.Models.API.Shows;
+using static System.Net.Mime.MediaTypeNames;
 
 namespace FMFT.Web.Client.Views.Pages.Home.Shows
 {
@@ -10,10 +11,25 @@ namespace FMFT.Web.Client.Views.Pages.Home.Shows
 
         public APIResponse<List<Show>> ShowsResponse { get; set; }
 
+        public List<Show> Shows => ShowsResponse.Object;
+
         protected override async Task OnInitializedAsync()
         {
             ShowsResponse = await APIBroker.GetAllShowsAsync();
             LoadingView.StopLoading();
+        }
+
+        private string GetShowThumbnailStyle(Show show)
+        {            
+            if (!show.ThumbnailMediaId.HasValue)
+            {
+                return string.Empty;
+            }
+
+            string styleFormat = "background-image: linear-gradient(rgba(0, 0, 0, 0.6), rgba(0, 0, 0, 0.6)), url({0});";
+            string thumbnailUrl = MediaService.GetMediaUrl(show.ThumbnailMediaId.Value);
+
+            return string.Format(styleFormat, thumbnailUrl);
         }
     }
 }
