c9186ec655d3ee6c0868f4e16619ce49de9c3dc1
MCrow <michalwrona@hotmail.com>
2022-11-23 17:40:53 +0100

    Add admin reservation page

---
+++ web/Client/Views/Pages/Admin/Reservations/ReservationAdminPage.razor.cs
@@ -0,0 +1,25 @@
+﻿using FMFT.Extensions.Blazor.Bases.Loadings;
+using FMFT.Web.Client.Models.API;
+using FMFT.Web.Client.Models.API.Reservations;
+using Microsoft.AspNetCore.Components;
+
+namespace FMFT.Web.Client.Views.Pages.Admin.Reservations
+{
+    public partial class ReservationAdminPage
+    {
+        [Parameter]
+        public string ReservationId { get; set; }
+
+        public LoadingView LoadingView { get; set; }
+
+        public APIResponse<Reservation> ReservationResponse { get; set; }
+
+        public Reservation Reservation => ReservationResponse.Object;
+
+        protected override async Task OnParametersSetAsync()
+        {
+            ReservationResponse = await APIBroker.GetReservationByIdAsync(ReservationId);
+            LoadingView.StopLoading();
+        }
+    }
+}
