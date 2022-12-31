ddd87b351734a71f888f10aa2c0e1ea7e99ada09
MCrow <michalwrona@hotmail.com>
2022-12-31 18:03:14 +0100

    Add navigation component base

---
+++ extensions/blazor/Bases/Navigations/Navigation.razor.cs
@@ -0,0 +1,58 @@
+﻿using FMFT.Extensions.Blazor.Bases.Steppers;
+using Microsoft.AspNetCore.Components;
+
+namespace FMFT.Extensions.Blazor.Bases.Navigations
+{
+    public partial class Navigation
+    {
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        public List<NavigationItem> Items { get; set; } = new();
+
+        public void AddItem(NavigationItem item)
+        {
+            Items.Add(item);
+
+            if (item.IsActive)
+            {
+                SetActiveItem(item);   
+            }
+        }
+
+        public NavigationItem ActiveItem { get; set; }
+        public Guid ActiveItemId => ActiveItem?.ID ?? Guid.Empty;
+
+        public void SetActiveItem(NavigationItem item)
+        {
+            if (ActiveItem == item)
+            {
+                return;
+            }
+
+            ActiveItem?.SetIsActive(false);
+            
+            ActiveItem = item;
+            ActiveItem.SetIsActive(true);
+
+            StateHasChanged();
+        }
+
+        public Task HandleClickAsync(NavigationItem item)
+        {
+            SetActiveItem(item);
+
+            return Task.CompletedTask;
+        }
+
+        public Task HandleSelectAsync(ChangeEventArgs args)
+        {
+            Guid itemId = Guid.Parse(args.Value.ToString());
+            NavigationItem item = Items.First(x => x.ID == itemId);
+
+            SetActiveItem(item);
+
+            return Task.CompletedTask;
+        }
+    }
+}
---
+++ extensions/blazor/Bases/Navigations/NavigationItem.razor.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace FMFT.Extensions.Blazor.Bases.Navigations
+{
+    public partial class NavigationItem
+    {
+        [Parameter]
+        public string Text { get; set; }
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+        [Parameter]
+        public bool Active { get; set; }
+
+        [CascadingParameter]
+        public Navigation Navigation { get; set; }
+
+        public Guid ID { get; } = Guid.NewGuid();
+
+        protected override void OnAfterRender(bool firstRender)
+        {
+            if (!firstRender)
+                return;
+
+            IsActive = Active;
+            Navigation.AddItem(this);
+        }
+
+        public bool IsActive { get; set; }
+
+        public void SetIsActive(bool isActive)
+        {
+            IsActive = isActive;
+        }
+
+        public string GetActiveClass()
+        {
+            return IsActive ? "active" : string.Empty;
+        }
+
+    }
+}
