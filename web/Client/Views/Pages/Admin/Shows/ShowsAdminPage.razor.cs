a619ddc61b67176fb85b3c8230fd9cbe0b7e79d4
MCrow <michalwrona@hotmail.com>
2023-01-01 19:04:57 +0100

    Improve admin shows page

--- web/Client/Views/Pages/Admin/Shows/ShowsAdminPage.razor.cs
+++ web/Client/Views/Pages/Admin/Shows/ShowsAdminPage.razor.cs
@@ -1,5 +1,6 @@
 ﻿using FMFT.Extensions.Blazor.Bases.Loadings;
 using FMFT.Web.Client.Models.API;
+using FMFT.Web.Client.Models.API.Auditoriums;
 using FMFT.Web.Client.Models.API.Shows;
 using FMFT.Web.Shared.Enums;
 
@@ -10,8 +11,10 @@ namespace FMFT.Web.Client.Views.Pages.Admin.Shows
         public LoadingView LoadingView { get; set; }
 
         public List<Show> Shows => ShowsResponse.Object;
+        public List<Auditorium> Auditoriums => AuditoriumsResponse.Object;
 
         public APIResponse<List<Show>> ShowsResponse { get; set; }
+        public APIResponse<List<Auditorium>> AuditoriumsResponse { get; set; }
 
         protected override async Task OnInitializedAsync()
         {
@@ -21,7 +24,19 @@ namespace FMFT.Web.Client.Views.Pages.Admin.Shows
             }
 
             ShowsResponse = await APIBroker.GetAllShowsAsync();
+            AuditoriumsResponse = await APIBroker.GetAllAuditoriumsAsync();
+
             LoadingView.StopLoading();
+        }
+
+        public string GetAuditoriumName(Show show)
+        {
+            return Auditoriums.FirstOrDefault(x => x.Id == show.AuditoriumId)?.Name ?? string.Empty;
+        }
+            
+        public int GetAuditoriumSeats(Show show)
+        {
+            return Auditoriums.FirstOrDefault(x => x.Id == show.AuditoriumId)?.Seats.Count ?? 0;
         }
 
         private string GetRowClass(Show show)
