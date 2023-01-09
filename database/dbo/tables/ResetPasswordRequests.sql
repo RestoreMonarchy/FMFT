85e5c785c2f54568cc91c53371b086ff5d8bea64
tomaszw <tomasz.wrona@hotmail.com>
2023-01-09 12:02:36 +0100

    Database project: 1) Change notation on computed columns to avoid recreating them on publish; 2) Change target db from 2016 to 2019; 3) Change database recovery model to SIMPLE

--- database/dbo/tables/ResetPasswordRequests.sql
+++ database/dbo/tables/ResetPasswordRequests.sql
@@ -4,9 +4,9 @@
 	UserId INT NOT NULL CONSTRAINT FK_ResetPasswordRequests_UserId FOREIGN KEY REFERENCES dbo.Users(Id),
 	SecretKey UNIQUEIDENTIFIER NOT NULL CONSTRAINT DF_ResetPasswordRequests_SecretKey DEFAULT (NEWID()),
 	ExpireDate DATETIME2(0) NOT NULL,
-	IsExpired AS CAST(IIF(ExpireDate > SYSDATETIME(), 0, 1) AS BIT),
+	IsExpired AS CONVERT(BIT,CASE WHEN [ExpireDate] > SYSDATETIME() THEN 0 ELSE 1 END),
 	ResetDate DATETIME2(0) NULL,
-	IsReset AS CAST(IIF(ResetDate IS NULL, 0, 1) AS BIT),
+	IsReset AS CONVERT(BIT,CASE WHEN ResetDate IS NULL THEN 0 ELSE 1 END),
 	CreateDate DATETIME2(0) NOT NULL CONSTRAINT DF_ResetPasswordRequests_CreateDate DEFAULT (SYSDATETIME()),
 	CONSTRAINT UK_ResetPasswordRequests UNIQUE (SecretKey)
 );
