a4667332ad6986f0180caa6cbaac1a77430432d4
MCrow <michalwrona@hotmail.com>
2023-01-28 16:36:55 +0100

    add custom payment status description on AccountOrderPage

--- web/Client/Views/Pages/Account/Orders/AccountOrderPage.razor.cs
+++ web/Client/Views/Pages/Account/Orders/AccountOrderPage.razor.cs
@@ -2,6 +2,7 @@
 using FMFT.Web.Client.Models.API;
 using FMFT.Web.Client.Models.API.Orders;
 using FMFT.Web.Client.Models.API.Reservations;
+using FMFT.Web.Shared.Enums;
 using Microsoft.AspNetCore.Components;
 
 namespace FMFT.Web.Client.Views.Pages.Account.Orders
@@ -18,6 +19,13 @@ namespace FMFT.Web.Client.Views.Pages.Account.Orders
 
         public Order Order => OrderResponse.Object;
         public List<Reservation> OrderReservations => OrderReservationsResponse.Object;
+        private Dictionary<OrderStatus, string> OrderStatusDescriptions { get; } = new()
+        {
+            { OrderStatus.Completed, "Zamówienie zostało zrealizowane" },
+            { OrderStatus.PaymentReceived, "Płatność za zamówienie została zarejestrowana" },
+            { OrderStatus.Expired, "Minął czas na dokonanie płatności. Zamówienie wygasło" },
+            { OrderStatus.PaymentWaiting, "Dokończ dokonywanie płatności na stronie operatora przed upływem czasu" }
+        };
 
         protected override async Task OnParametersSetAsync()
         {
@@ -29,7 +37,22 @@ namespace FMFT.Web.Client.Views.Pages.Account.Orders
             OrderResponse = await APIBroker.GetOrderByIdAsync(OrderId);
             OrderReservationsResponse = await APIBroker.GetOrderReservationsByIdAsync(OrderId);
 
+            if (OrderResponse.IsSuccessful)
+            {
+                // Expired but actually not
+                if (Order.IsExpired && Order.Status == OrderStatus.PaymentWaiting)
+                {
+                    Order.Status = OrderStatus.Expired;
+                }
+            }
+
             LoadingView.StopLoading();
+        }
+
+        private void HandleTimeElapsed()
+        {
+            Order.IsExpired = true;
+            Order.Status = OrderStatus.Expired;
         }
     }
 }
