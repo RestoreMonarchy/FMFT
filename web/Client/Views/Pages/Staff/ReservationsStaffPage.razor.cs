ed41492b215ab30550787d78b12ec1c138c88fd2
MCrow <michalwrona@hotmail.com>
2023-03-02 00:21:01 +0100

    start working on Reservations staff page

---
+++ web/Client/Views/Pages/Staff/ReservationsStaffPage.razor.cs
@@ -0,0 +1,22 @@
+﻿using FMFT.Extensions.Blazor.Bases.Alerts;
+using FMFT.Extensions.Blazor.Bases.Loadings;
+using Microsoft.AspNetCore.Components;
+
+namespace FMFT.Web.Client.Views.Pages.Staff
+{
+    public partial class ReservationsStaffPage
+    {
+        [Parameter]
+        public string ReservationId { get; set; }
+
+        public AlertGroupBase AlertGroup { get; set; }
+        public AlertBase NotFoundAlert { get; set; }
+        public AlertBase SuccessAlert { get; set; }
+        public LoadingView LoadingView { get; set; }
+
+        protected override async Task OnParametersSetAsync()
+        {
+
+        }
+    }
+}
