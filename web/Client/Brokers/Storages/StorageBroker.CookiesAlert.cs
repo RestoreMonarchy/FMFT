558298c9d9b01dfdcb61aaba0a6d73103d6bb057
MCrow <michalwrona@hotmail.com>
2024-02-25 22:31:13 +0100

    Add cookies alert

---
+++ web/Client/Brokers/Storages/StorageBroker.CookiesAlert.cs
@@ -0,0 +1,15 @@
+﻿namespace FMFT.Web.Client.Brokers.Storages
+{
+    public partial class StorageBroker
+    {
+        public async ValueTask<bool> GetCookiesAlertFlagAsync()
+        {
+            return await GetLocalItemAsync<bool>("CookiesAlert");
+        }
+
+        public async ValueTask SetCookiesAlertFlagAsync(bool value)
+        {
+            await SetLocalItemAsync("CookiesAlert", value);
+        }
+    }
+}
---
+++ web/Client/Views/Shared/Layouts/Main/MainLayout.razor.cs
@@ -0,0 +1,36 @@
+﻿using FMFT.Web.Client.Brokers.Storages;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+
+namespace FMFT.Web.Client.Views.Shared.Layouts.Main
+{
+    public partial class MainLayout
+    {
+
+        public ErrorBoundary ErrorBoundary { get; set; }
+
+        private bool displayCookies = false;
+
+        protected override void OnParametersSet()
+        {
+            ErrorBoundary?.Recover();    
+        }
+
+        protected override async Task OnInitializedAsync()
+        {
+            bool cookiesAlertFlag = await StorageBroker.GetCookiesAlertFlagAsync();
+            displayCookies = !cookiesAlertFlag;
+        }
+
+        protected override async Task OnParametersSetAsync()
+        {
+            await JSRuntimeBroker.ClearModalBackdropAsync();
+        }
+
+        private async Task HideCookiesAlertAsync()
+        {
+            displayCookies = false;
+            await StorageBroker.SetCookiesAlertFlagAsync(true);
+        }
+    }
+}
